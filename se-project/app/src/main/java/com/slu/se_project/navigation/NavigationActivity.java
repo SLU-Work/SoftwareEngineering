8b908d6f5ad8645800a704d3aadfe895752ca55e
Bhargavi Kullakanda <bhargavi.kullakanda@gmail.com>
2017-03-27 14:28:46 -0500

    SE4 - Added the toolbar at the top of all app pages [BK]

--- se-project/app/src/main/java/com/slu/se_project/navigation/NavigationActivity.java
+++ se-project/app/src/main/java/com/slu/se_project/navigation/NavigationActivity.java
@@ -1,9 +1,13 @@
 package com.slu.se_project.navigation;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.slu.se_project.PlanitApplication;
 import com.slu.se_project.R;
@@ -22,6 +26,8 @@ public abstract class NavigationActivity extends AppCompatActivity {
     private NavigationController mNavigationController;
     private Toolbar mNavigationToolbar;
     private FrameLayout mContentContainer;
+//    private TextView mNavMenuTitle;
+    private DrawerLayout mNavigationDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +36,14 @@ public abstract class NavigationActivity extends AppCompatActivity {
 
 
         mNavigationToolbar = (Toolbar)findViewById(R.id.navigation_toolbar);
+        mContentContainer = (FrameLayout)findViewById(R.id.navigation_activity_container);
+        mNavigationDrawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+//        mNavMenuTitle = (TextView) findViewById(R.id.toolbar_title);
 
         setSupportActionBar(mNavigationToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
 
-        mContentContainer = (FrameLayout)findViewById(R.id.navigation_activity_container);
         if(getContentResourceId()!=0){mContentContainer.addView(getLayoutInflater().inflate(getContentResourceId(),null));}
 
         mPlanitApplication = PlanitApplication.getInstance();
@@ -46,6 +55,10 @@ public abstract class NavigationActivity extends AppCompatActivity {
         setupViewHandles();
         setupViewListeners();
     }
+
+//    protected TextView getNavMenuTitle(){return mNavMenuTitle;}
+
+    protected DrawerLayout getNavigationDrawer(){return mNavigationDrawer;}
 
     protected SessionManager getSessionManager(){return mSessionManager;}
 
@@ -68,7 +81,24 @@ public abstract class NavigationActivity extends AppCompatActivity {
 
     protected void setupViewHandles() {}
 
-    protected void setupViewListeners() {}
+    protected void setupViewListeners() {
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mNavigationDrawer, mNavigationToolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        mNavigationDrawer.addDrawerListener(actionBarDrawerToggle);
+    }
 
     protected abstract int getContentResourceId();
 
