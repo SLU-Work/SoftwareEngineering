9d10d95017695c2510cd8f2cabaace059a9dd0d1
Bhargavi Kullakanda <bhargavi.kullakanda@gmail.com>
2017-03-29 15:26:15 -0500

    SE7 - Added connection from Serach option in menu to SearchActivity [BK]

--- se-project/app/src/main/java/com/slu/se_project/navigation/NavigationScreens.java
+++ se-project/app/src/main/java/com/slu/se_project/navigation/NavigationScreens.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 
 import com.slu.se_project.login.LoginActivity;
 import com.slu.se_project.main.MainActivity;
+import com.slu.se_project.search.SearchActivity;
 import com.slu.se_project.userProfile.UserProfileActivity;
 
 /**
@@ -15,12 +16,13 @@ public enum NavigationScreens {
     LOGIN_VIEW(LoginActivity.class),
 //    EULA_VIEW(EulaActivity.class),
     MAIN_VIEW(MainActivity.class),
-    USER_PROFILE_VIEW(UserProfileActivity.class);
 //    INSTALL_TSTAT_VIEW(InstallationActivity.class),
 //    ACCOUNT_VIEW(PersonalInfoActivity.class),
 //    EMAIL_PREFS_VIEW(EmailPreferencesActivity.class),
 //    FEEDBACK_VIEW(FeedbackActivity.class),
 //    DASHBOARD_VIEW(DashboardActivity.class);
+    USER_PROFILE_VIEW(UserProfileActivity.class),
+    SEARCH_VIEW(SearchActivity.class);
 
     private Class<? extends Activity> mActivity;
 
